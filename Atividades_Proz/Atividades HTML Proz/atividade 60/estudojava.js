3a2424067c915e77a4d55ae377366b6f554989b6
Marcus <146457088+MarcusOloski@users.noreply.github.com>
2023-11-22 11:49:01 -0300

    Add files via upload

---
+++ Atividades_Proz/Atividades HTML Proz/atividade 60/estudojava.js
@@ -0,0 +1,11 @@
+/*var resposta;
+nome = prompt("What is your name?");
+alert(nome + ", we have a city to burn!");*/
+var idade = prompt("Quantos anos você tem?")
+if (idade < 18) {
+    alert ("vai tomar no bobba");
+} else if (idade < 20) {
+    alert("vai tomar no cu");
+} else {
+    alert("Idoso. Ta morrendo já.");
+}
